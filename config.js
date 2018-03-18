00c607c737f887c5c923e670674692d651436eb0
zengluqiang <zengluqiang@dafy.com>
2018-03-18 17:43:45 +0800

    增加 配置文件 config.js

---
+++ config.js
@@ -0,0 +1,41 @@
+// var src = './src';
+var src = 'app';
+var dest = 'dist';
+// 请勿 修改 rev -- 没有写
+// less 和 sass 都在 dest + "/css" -- 没有写
+
+module.exports = {
+	src: src,
+	dest: dest,
+	less: {
+		all: src + "/less/**/*.less", // 所有 less
+        src: src + "/less/*.less", // 需要编译的 less
+        dest: dest + "/css", // 输出目录
+        rev: dest + "/rev/css",
+        settings: { // 编译 less 过程需要的配置，可以为空
+
+        }
+	},
+	sass: {
+		all: src + "/scss/**/*.scss", // 所有scss
+        src: src + "/scss/*.scss", // 需要编译的scss
+        dest: dest + "/css", // 输出目录
+        rev: dest + "/rev/css",
+        settings: { // 编译 scss 过程需要的配置，可以为空
+
+        }
+	},
+	js: {
+		src: src + "/js/**/*.js",
+        dest: dest + "/js",
+        rev: dest + "/rev/js"
+	},
+	html: {
+		src: src + "/**/*.html",
+        dest: dest,
+        ignore: ["!"+src+"/publicHTML/*.html"]
+	},
+	rev: {
+
+	}
+};
--- gulpfile.js
+++ gulpfile.js
@@ -22,33 +22,36 @@ var zip          = require('gulp-zip');
 var clean        = require('gulp-clean');
 // 同步执行任务 -- gulp 的任务的执行是异步的。 
 var runSequence  = require('run-sequence');
+// 配置文件
+var config       = require('./config');
+console.log(config);
 
 gulp.task('serve', function() {
     // BrowserSync 监听 dist 目录
     browserSync.init({
         server: {
-            baseDir:["./dist"],
+            baseDir:[config.dest],
             middleware:SSI({
-                baseDir:'./dist',
-                ext:'.shtml',
-                version:'2.10.0'
+                baseDir: config.dest,
+                ext: '.shtml',
+                version: '2.10.0'
             })
         }
     });
     // watch -- 将文件编译到 dist 目录下, 触发 BrowserSync
-    gulp.watch("app/scss/**/*.scss", ['sass']);
-    gulp.watch("app/less/**/*.less", ['less']);
-    gulp.watch("app/scripts/**/*.js", ['js']);
-    gulp.watch("app/**/*.html", ['html']);
+    gulp.watch(config.sass.all, ['sass']);
+    gulp.watch(config.less.all, ['less']);
+    gulp.watch(config.js.src, ['js']);
+    gulp.watch(config.html.src, ['html']);
     // browserSync.reload -- 浏览器重载
-    gulp.watch("dist/**/*.html").on("change", browserSync.reload);
+    gulp.watch(config.html.dest).on("change", browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 // 编译SASS 且 自动注入到浏览器
 gulp.task('sass', function() {
     // browserSync.stream() -- 该 stream 方法返回一个变换流，并且可以充当一次或多个文件。
-    return gulp.src("app/scss/**/*.scss")
+    return gulp.src(config.sass.all)
         // .pipe(plumber())
         .pipe(plumber({errorHandler:notify.onError('Error:<%=error.message%>')}))
         .pipe(sass.sync().on('error', sass.logError))
@@ -64,12 +67,12 @@ gulp.task('sass', function() {
         .pipe(cssmin())
         // 兼容 IE7 及 以下需设置 compatibility 属性
         // .pipe(cssmin({compatibility: 'ie7'}))
-        .pipe(gulp.dest("dist/styles"))
+        .pipe(gulp.dest(config.sass.dest))
         .pipe(browserSync.stream());
 });
 
 gulp.task('less', function() {
-    return gulp.src("app/less/**/*.less")
+    return gulp.src(config.less.all)
         .pipe(plumber({errorHandler:notify.onError('Error:<%=error.message%>')}))
         .pipe(less())
         // .pipe(autoprefixer())
@@ -82,59 +85,58 @@ gulp.task('less', function() {
         .pipe(cssmin())
         // 兼容 IE7 及 以下需设置 compatibility 属性
         // .pipe(cssmin({compatibility: 'ie7'}))
-        .pipe(gulp.dest("dist/styles"))
+        .pipe(gulp.dest(config.less.dest))
         .pipe(browserSync.stream());
 });
 
 // javscript files operate
 gulp.task('js', function(){
     // minify() -- gulp-minify 压缩 js 代码
-    return gulp.src('app/scripts/**/*.js')
+    return gulp.src(config.js.src)
         .pipe(plumber({errorHandler:notify.onError('Error:<%=error.message%>')}))
         .pipe(minify())
-        .pipe(gulp.dest("dist/scripts"))
+        .pipe(gulp.dest(config.js.dest))
         .pipe(browserSync.stream());
 });
 
 // 处理 html 文件
 gulp.task('html', function() {
     // 排除 publicHTML 下的 html
-    return gulp.src(["app/*.html", "!app/publicHTML/*.html"])
+    return gulp.src([config.html.src].concat(config.html.ignore))
         .pipe(plumber({errorHandler:notify.onError('Error:<%=error.message%>')}))
         .pipe(fileinclude({
           prefix: '@@',
           basepath: '@file'
         }))
-        .pipe(gulp.dest("dist/"))
+        .pipe(gulp.dest(config.html.dest))
         .pipe(browserSync.stream());
 });
 
 // CSS生成文件hash编码 并 生成 rev-manifest.json文件名对照映射
 gulp.task('revCss', function() {
-
-    return gulp.src("dist/styles/**/*.css")
+    return gulp.src(config.dest+"/css/**/*.css")
         // .pipe(plumber())
         .pipe(plumber({errorHandler:notify.onError('Error:<%=error.message%>')}))
         .pipe(rev()) // 设置 hash 值
-        .pipe(gulp.dest('dist/styles'))
+        .pipe(gulp.dest(config.dest+"/css"))
         .pipe(rev.manifest()) // 生产 hash 值得 json 文件
-        .pipe(gulp.dest('dist/rev/css')); // 保存 hash 值得 json 文件
+        .pipe(gulp.dest(config.dest+'/rev/css')); // 保存 hash 值得 json 文件
 });
 
 //Html替换css、js文件版本
 gulp.task('revHtml', function () {
-    return gulp.src(["dist/rev/**/*.json", "dist/*.html"])
+    return gulp.src([config.dest+"/rev/**/*.json", config.dest+"/*.html"])
         .pipe(plumber({errorHandler:notify.onError('Error:<%=error.message%>')}))
         .pipe(revCollector({ // Html -- 替换 css、js文件版本
             replaceReved: true
         }))
-        .pipe(gulp.dest("dist/"));
+        .pipe(gulp.dest(config.dest));
 });
 
 // 清空目标文件
 gulp.task('clean', function () {
     console.log('clean');
-    return gulp.src(['dist'], {read: false})
+    return gulp.src([config.dest], {read: false})
         .pipe(clean());
 });
 
@@ -142,7 +144,7 @@ gulp.task('clean', function () {
 gulp.task('publish', function() {
     // dist css 和 js hash
 
-    return gulp.src('dist/**/*')
+    return gulp.src(config.dest+'/**/*')
         .pipe(plumber({errorHandler:notify.onError('Error:<%=error.message%>')}))
         .pipe(zip('publish.zip'))
         .pipe(gulp.dest('release'))
@@ -170,4 +172,3 @@ gulp.task('dev', function (done) {
 
 // 编辑默认任务
 gulp.task('default', ['dev']);
-
